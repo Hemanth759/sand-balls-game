b9d9b74fb8f483195e276f77028da9f78ee4791e
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-05-28 00:08:47 +0530

    Removed and added new 3d game

---
+++ Sand Ball/Assets/Scripts/PlaneDeformer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDeformer : MonoBehaviour
+{
+    //public references
+    public float radiusOfDeformation;
+    public float powerOfDeformation;
+
+    // private references
+    MeshFilter meshFilter;
+    Mesh mesh;
+    Vector3[] verts;
+
+    private void Start()
+    {
+        meshFilter = this.GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+        verts = mesh.vertices;
+    }
+
+    public void deformThePlane(Vector3 positionToDeform)
+    {
+        positionToDeform = this.transform.InverseTransformPoint(positionToDeform);
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            float dist = (verts[i] - positionToDeform).sqrMagnitude;
+
+            if (dist < radiusOfDeformation)
+            {
+                verts[i] -= Vector3.up * powerOfDeformation;
+            }
+
+            mesh.vertices = verts;
+        }
+    }
+}
---
+++ Sand Ball/Assets/Scripts/TouchController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class TouchController : MonoBehaviour
+{
+    // public references
+
+    // private references
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera cam;
+    int temp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = 0;
+        cam = this.transform.GetComponent<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("deforming " + temp);
+            temp++;
+            // doformMesh();
+        }
+    }
+
+    private void doformMesh()
+    {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            PlaneDeformer sandPlane;
+            if (hit.transform.TryGetComponent<PlaneDeformer>(out sandPlane))
+                sandPlane.deformThePlane(hit.point);
+        }
+    }
+}
